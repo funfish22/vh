df610241f02fb1e7adddb17060b6e6592bad236f
funfish22 <j26391442@gmail.com>
2019-05-05 16:32:21 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+var open = document.querySelector('.open'),
+    close = document.querySelector('.close'),
+    body = document.body,
+    lightBox = document.querySelector('.my-element'),
+    vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+open.addEventListener('click', function() {
+    body.style.overflow = 'hidden'
+    body.style.position = 'fixed'
+    body.style.width = '100%'
+    body.style.height = '100%'
+
+    lightBox.style.display = 'block'
+}, false)
+
+close.addEventListener('click', function() {
+    body.style.overflow = ''
+    body.style.position = ''
+    body.style.width = ''
+    body.style.height = ''
+
+    lightBox.style.display = ''
+})
+
+window.addEventListener('resize', function() {
+    // We execute the same script as before
+    var vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
